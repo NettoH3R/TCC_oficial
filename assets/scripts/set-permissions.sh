ae2c2f36d9168a51e2cf628ea841003db05402f4
marcoweb <marcocarvalho.web@gmail.com>
2023-08-23 20:34:08 +0000

    Configure Docker

--- assets/scripts/set-permissions.sh
+++ assets/scripts/set-permissions.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker exec php sh -c 'chown -R www-data /var/www/html'
+docker exec php sh -c 'chown -R www-data /var/www/html/arquivos'
+docker exec php sh -c 'chown -R www-data /var/www/html/artistas'
