3803b0ad68072de1fb5b054e512c310df9b3efb7
luaanHD <luantoniolo94@gmail.com>
2023-08-28 17:55:05 +0000

    Inicio da dor de cabeça que é o sobre.php

--- app/sobre.php
+++ app/sobre.php
@@ -1,8 +1,33 @@
  <?php
-    $title = 'sobre';
+    $title = 'Sobre';
     include('includes/header.php');
     ?>
 
-<?php
-include('includes/footer.php');
-?>
+ <div class="container">
+
+     <main class="main">
+
+         <div class="row">
+
+             <div class="col-md-3 sobreRetangulo" style="background-color: #00e1ff;">
+                 <p>a</p>
+             </div>
+
+             <div class="col-md-3 sobreRetangulo" style="background-color: #00e1ff;">
+                 <p>a</p>
+             </div>
+
+             <div class="col-md-3 sobreRetangulo" style="background-color: #00e1ff;">
+                 <p>a</p>
+             </div>
+
+         </div>
+
+     </main>
+
+ </div>
+
+
+ <?php
+    include('includes/footer.php');
+    ?>
