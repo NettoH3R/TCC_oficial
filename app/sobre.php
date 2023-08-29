03eec451c0975d695cd829f7981e55a5ff41613a
luaanHD <luantoniolo94@gmail.com>
2023-08-29 23:32:09 +0000

    sobre.php pronto??

--- app/sobre.php
+++ app/sobre.php
@@ -6,12 +6,25 @@ include('includes/header.php');
 <div class="container">
     <main class="main" style="padding-top: 0;">
 
+        <h1 class="text-center" style="padding-top: 50px;margin-bottom: -20px;font-family: 'Rubik', sans-serif;font-weight: bold; color: #ff8f2d; text-shadow: -1.5px -1.5px 1.5px black, 1.5px -1.5px 1.5px black, -1.5px 1.5px 1.5px black, 1.5px 1.5px 1.5px black; justify-content: center; align-items: center;">Membros do UrderSounds</h1>
+
         <div class="row" style="padding: 0; margin: 0;">
 
             <div class="col-md-4">
                 <div class="sobre-quadradinho">
                     <div class="foto-sobre">
+                        <img src="arquivos/imagensSobre/felipeSobre.jpeg" class="fotoNossaSobre" alt="Foto do Felipe Siqueira Camargo">
+                    </div>
+                    <div class="textoSobre">
+                        <p><span class="textoSobreBold">Nome:</span> Felipe Siqueira Camargo</p>
 
+                        <p><span class="textoSobreBold">Idade:</span> 18 anos</p>
+
+                        <p><span class="textoSobreBold"> Função:</span> Design</p>
+
+                        <p><span class="textoSobreBold"> Escola:</span> ETEC Mário Antônio Verza</p>
+
+                        <p><span class="textoSobreBold"> Ano escolar:</span> 3° ETIM – Informática para Internet</p>
                     </div>
                 </div>
             </div>
@@ -19,7 +32,18 @@ include('includes/header.php');
             <div class="col-md-4">
                 <div class="sobre-quadradinho">
                     <div class="foto-sobre">
+                        <img src="arquivos/imagensSobre/luanSobre.jpeg" class="fotoNossaSobre" alt="Foto do Luan Martins Toniolo">
+                    </div>
+                    <div class="textoSobre">
+                        <p><span class="textoSobreBold">Nome:</span> Luan Martins Toniolo</p>
 
+                        <p><span class="textoSobreBold">Idade:</span> 17 anos</p>
+
+                        <p><span class="textoSobreBold">Função:</span> Designer Front-End / Assistente Geral</p>
+
+                        <p><span class="textoSobreBold">Escola:</span> ETEC Mário Antônio Verza</p>
+
+                        <p><span class="textoSobreBold">Ano escolar:</span> 3° ETIM – Informática para Internet</p>
                     </div>
                 </div>
             </div>
@@ -27,7 +51,18 @@ include('includes/header.php');
             <div class="col-md-4">
                 <div class="sobre-quadradinho">
                     <div class="foto-sobre">
+                        <img src="arquivos/imagensSobre/nettoSobre.jpeg" class="fotoNossaSobre" alt="Foto do João Victor Netto Araújo">
+                    </div>
+                    <div class="textoSobre">
+                        <p><span class="textoSobreBold">Nome:</span> João Victor Netto Araújo</p>
 
+                        <p><span class="textoSobreBold">Idade:</span> 17 anos</p>
+
+                        <p><span class="textoSobreBold">Função:</span> Desenvolvedor de Back-End</p>
+
+                        <p><span class="textoSobreBold">Escola:</span> ETEC Mário Antônio Verza</p>
+
+                        <p><span class="textoSobreBold">Ano escolar:</span> 3° ETIM – Informática para Internet </p>
                     </div>
                 </div>
             </div>
@@ -40,7 +75,18 @@ include('includes/header.php');
             <div class="col-md-4">
                 <div class="sobre-quadradinho">
                     <div class="foto-sobre">
+                        <img src="arquivos/imagensSobre/renanSobre.jpeg" class="fotoNossaSobre" alt="Foto do Renan Costa Leão">
+                    </div>
+                    <div class="textoSobre">
+                        <p><span class="textoSobreBold"> Nome:</span> Renan Costa Leão</p>
 
+                        <p><span class="textoSobreBold">Idade:</span> 18 anos</p>
+
+                        <p><span class="textoSobreBold">Função:</span> Design</p>
+
+                        <p><span class="textoSobreBold">Escola:</span> ETEC Mário Antônio Verza</p>
+
+                        <p><span class="textoSobreBold">Ano escolar:</span> 3° ETIM – Informática para Internet</p>
                     </div>
                 </div>
             </div>
@@ -48,7 +94,18 @@ include('includes/header.php');
             <div class="col-md-4">
                 <div class="sobre-quadradinho">
                     <div class="foto-sobre">
+                        <img src="arquivos/imagensSobre/miguelzaoSobre.jpeg" class="fotoNossaSobre" alt="Foto do Miguel Renato Silva Campos">
+                    </div>
+                    <div class="textoSobre">
+                        <p><span class="textoSobreBold">Nome:</span> Miguel Renato Silva Campos</p>
 
+                        <p><span class="textoSobreBold">Idade:</span> 17 anos</p>
+
+                        <p><span class="textoSobreBold">Função:</span> Design</p>
+
+                        <p><span class="textoSobreBold">Escola:</span> ETEC Mário Antônio Verza</p>
+
+                        <p><span class="textoSobreBold">Ano escolar:</span> 3° ETIM – Informática para Internet</p>
                     </div>
                 </div>
             </div>
