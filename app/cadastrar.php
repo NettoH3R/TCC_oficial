497d509ef20e8894988b910fe0e00a9c3213021d
João Netto <jovinettonetto@gmail.com>
2023-09-05 14:19:34 +0000

    Cadastre-se e Entrar prontos

--- app/cadastrar.php
+++ app/cadastrar.php
@@ -3,8 +3,46 @@ $title = 'Cadastre-se';
 include('includes/noHeader.php');
 ?>
 <div class="container">
-    <main class="main fundoCadastrar">
-        <div class="fundoCadastrar">
+<main class="main" style="width: 100%;">
+        <div class="row">
+            <div class="col-md-1 col-sm-1">
+
+            </div>
+
+            <!-- IMAGEM DE FUNDO -->
+
+            <div class="col-md-10 backImage col-sm-10">
+                <div class="row">
+
+
+                    <!-- PRIMEIRA METADE -->
+                    <div class="col-md-6 quadradoEntrar col-sm-12">
+                        <img src="/arquivos/imagensDoSite/logoEntrar.png" alt="" class="logoEntrar">
+                    </div>
+
+                    <!-- SEGUNDA METADE -->
+                    <div class="col-md-6 quadrado2Entrar col-sm-12">
+
+                        <h2 style="padding-top: 5%; font-size: 24pt; text-align: center; color: white; font-weight: bold;">Bem-Vindo!</h2>
+
+                        <form action="" method="post">
+                        <input name="nome" type="text" placeholder="Nome de Usuário" class="nomeDaFaixa" style=" width:80% ;  margin-top: 5%; margin-left: 10%; margin-right: 10%;" required> 
+                        <input name="nome" type="text" placeholder="E-mail" class="nomeDaFaixa" style=" width:80% ;  margin-top: 5%; margin-left: 10%; margin-right: 10%;" required> 
+                        <input name="nome" type="password" placeholder="Senha" class="nomeDaFaixa" style=" width:80% ;  margin-top: 5%; margin-left: 10%; margin-right: 10%;" required>
+                        <input name="nome" type="password" placeholder="Confirme a Senha" class="nomeDaFaixa" style=" width:80% ;  margin-top: 5%; margin-left: 10%; margin-right: 10%;" required>
+                        <button type="submit" class="bnt-entrar-entrar">Cadastre-se</button>
+                        </form>
+
+                        <p style="text-align: center; color: white; font-size: 14pt; margin-top: 2%;">Já tem uma conta UnderSounds?</p>
+                        <p style="text-align: center; color: white; font-size: 14pt; margin-top: -4%;"><a class="sublinhado" href="entrar.php">ENTRE COM SUA CONTA</a></p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-1 col-sm-1">
+
+            </div>
         </div>
     </main>
 </div>
@@ -15,7 +53,6 @@ include('includes/noHeader.php');
     </footer>
 </div>
 
-</main>
 </body>
 
 </html>
--- app/entrar.php
+++ app/entrar.php
@@ -3,35 +3,42 @@ $title = 'Entrar';
 include('includes/noHeader.php');
 ?>
 <div class="container">
-    <main class="main" style="width: 100%; height: 85vh;">
+    <main class="main" style="width: 100%;">
         <div class="row">
-            <div class="col-md-2">
+            <div class="col-md-1 col-sm-1">
 
             </div>
 
             <!-- IMAGEM DE FUNDO -->
 
-            <div class="col-md-8 backImage">
+            <div class="col-md-10 backImage col-sm-10">
                 <div class="row">
 
 
                     <!-- PRIMEIRA METADE -->
-                    <div class="col-md-6 quadradoEntrar">
+                    <div class="col-md-6 quadradoEntrar col-sm-12">
                         <img src="/arquivos/imagensDoSite/logoEntrar.png" alt="" class="logoEntrar">
                     </div>
 
                     <!-- SEGUNDA METADE -->
-                    <div class="col-md-6 quadrado2Entrar">
+                    <div class="col-md-6 quadrado2Entrar col-sm-12">
+
+                        <h2 style="padding-top: 10%; font-size: 24pt; text-align: center; color: white; font-weight: bold;">Bem-Vindo de Volta!</h2>
+
                         <form action="" method="post">
-                        <input name="nome" type="text" placeholder="Nome ou E-mail" class="nomeDaFaixa" style="margin-bottom: 0px;" required> 
-                        <input name="nome" type="text" placeholder="Senha" class="nomeDaFaixa" required>
+                        <input name="nome" type="text" placeholder="Nome ou E-mail" class="nomeDaFaixa" style=" width:80% ;  margin-top: 10%; margin-left: 10%; margin-right: 10%;" required> 
+                        <input name="nome" type="password" placeholder="Senha" class="nomeDaFaixa" style=" width:80% ;  margin-top: 5%; margin-left: 10%; margin-right: 10%;" required>
+                        <button type="submit" class="bnt-entrar-entrar">Entrar</button>
                         </form>
+
+                        <p style="text-align: center; color: white; font-size: 14pt; margin-top: 8%;">Não tem uma conta UnderSounds?</p>
+                        <p style="text-align: center; color: white; font-size: 14pt; margin-top: -4%;"><a class="sublinhado"  href="cadastrar.php">CADASTRE-SE</a> AGORA </p>
                     </div>
 
                 </div>
             </div>
 
-            <div class="col-md-2">
+            <div class="col-md-1 col-sm-1">
 
             </div>
         </div>
