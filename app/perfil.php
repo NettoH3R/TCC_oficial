799d22967056c9ea48843d2386e9087338e53906
João Netto <jovinettonetto@gmail.com>
2023-09-07 15:27:43 +0000

    Perfil.php

--- app/perfil.php
+++ app/perfil.php
@@ -2,8 +2,19 @@
 include('includes/header.php');
 ?>
 
+<div class="container">
+    <main class="main" style="padding-top: 0;">
+
+        <!-- BANNER E IMAGEM DE PERFIL -->
+        <div class="banner-artista">
+            <img src="arquivos/imagensDoSite/textPura.jpg" alt="Banner" class="img-banner">
+            <div class="foto-artista">
+
+            </div>
+        </div>
+</div>
 
 
-<?php 
-include('includes/header.php');
+<?php
+include('includes/footer.php');
 ?>
