1a3d5c6f70f58aa2be3ce5455d2c30014edd0534
luaanHD <luantoniolo94@gmail.com>
2023-09-09 00:28:38 +0000

    deu dor de cabeça parei

--- app/perfil.php
+++ app/perfil.php
@@ -8,8 +8,9 @@ include('includes/header.php');
         <!-- BANNER E IMAGEM DE PERFIL -->
         <div class="banner-artista">
             <img src="arquivos/imagensDoSite/textPura.jpg" alt="Banner" class="img-banner">
-            <div class="foto-artista">
-
+            <div class="foto-artista" style="position: absolute; margin-left: 5%">
+                <div>
+                </div>
             </div>
         </div>
 </div>
