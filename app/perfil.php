5090342b41f7c7f4400fde925cb5ca4fed15b2a1
luaanHD <luantoniolo94@gmail.com>
2023-09-09 23:02:59 +0000

    falta detalhes no perfil.php

--- app/perfil.php
+++ app/perfil.php
@@ -8,8 +8,25 @@ include('includes/header.php');
         <!-- BANNER E IMAGEM DE PERFIL -->
         <div class="banner-artista">
             <img src="arquivos/imagensDoSite/textPura.jpg" alt="Banner" class="img-banner">
-            <div class="foto-artista" style="position: absolute; margin-left: 5%">
-                <div>
+
+            <div class="row" style="padding: 0; margin: 0;">
+
+                <div class="col-md-3 col-sm-3">
+                    <div class="foto-artista">
+                    </div>
+                </div>
+
+                <div class="col-md-2 col-sm-2">
+                    <div class="">
+                        <p class="nomeDoArtista">Renê</p>
+                       
+                    </div>
+                </div>
+
+                <div class="col-md-2 col-sm-2">
+                    <div class="">
+                        <button type="button" class="botaoSeguir">Seguir+</button>
+                    </div>
                 </div>
             </div>
         </div>
