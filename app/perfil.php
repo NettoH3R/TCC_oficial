37a2acfabb45d3bb36fdbff5dcd85254f7136969
João Netto <jovinettonetto@gmail.com>
2023-09-11 01:24:38 +0000

    Perfil Pronto

--- app/perfil.php
+++ app/perfil.php
@@ -11,26 +11,99 @@ include('includes/header.php');
 
             <div class="row" style="padding: 0; margin: 0;">
 
-                <div class="col-md-3 col-sm-3">
+                <div class="col-md-2 col-sm-2" style="overflow: hidden;">
                     <div class="foto-artista">
+                        <img class="ft-artista" src="./arquivos/imagensAlbuns/teste4.png" alt="">
                     </div>
                 </div>
 
-                <div class="col-md-2 col-sm-2">
-                    <div class="">
-                        <p class="nomeDoArtista">Renê</p>
-                       
+                <!-- dados do banner -->
+
+                <div class="col-md-10 col-sm-10">
+                    <div class="row" style="margin-top: -60px">
+                        <div class="col-md-3">
+                            <div class="nomeDoArtista">
+                                <p>Renê</p>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <button class="btn-seguir" type="button" class="botaoSeguir">Seguir +</button>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="infosArtista" style="margin-left: 10px;">
+                                <p>2222 Seguidores</p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-2">
+                            <div class="infosArtista">
+                                <p>29 Faixas</p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-2">
+                            <div class="infosArtista">
+                                <p>4 Albúns</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-md-2 col-sm-2">
-                    <div class="">
-                        <button type="button" class="botaoSeguir">Seguir+</button>
+            </div>
+        </div>
+        <!-- Parte de baixo rodapé -->
+        <div style="height: 100px;">
+
+        </div>
+
+        <div class="faixas">
+
+            <p style="font-size: 24pt; font-weight: bold; color: black;">Faixas</p>
+            <div class="scrool">
+                <div class="quadradinhos">
+                    <div class="quadradinho">
+                        <img src="./arquivos/imagensAlbuns/teste1.png" class="album" alt="Possível álbum a ser colocado">
+                    </div>
+                    <div class="quadradinho">
+                        <img src="./arquivos/imagensAlbuns/teste2.png" class="album" alt="Possível álbum a ser colocado">
+                    </div>
+                    <div class="quadradinho">
+                        <img src="./arquivos/imagensAlbuns/teste3.png" class="album" alt="Possível álbum a ser colocado">
+                    </div>
+                    <div class="quadradinho">
+                        <img src="./arquivos/imagensAlbuns/teste4.png" class="album" alt="Possível álbum a ser colocado">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<!-- ALBUNS -->
+
+        <div class="faixas">
+
+            <p style="font-size: 24pt; font-weight: bold; color: black;">Albúns</p>
+            <div class="scrool">
+                <div class="quadradinhos">
+                    <div class="quadradinho">
+                        <img src="./arquivos/imagensAlbuns/teste1.png" class="album" alt="Possível álbum a ser colocado">
+                    </div>
+                    <div class="quadradinho">
+                        <img src="./arquivos/imagensAlbuns/teste2.png" class="album" alt="Possível álbum a ser colocado">
+                    </div>
+                    <div class="quadradinho">
+                        <img src="./arquivos/imagensAlbuns/teste3.png" class="album" alt="Possível álbum a ser colocado">
+                    </div>
+                    <div class="quadradinho">
+                        <img src="./arquivos/imagensAlbuns/teste4.png" class="album" alt="Possível álbum a ser colocado">
                     </div>
                 </div>
             </div>
         </div>
 </div>
+</div>
+
+
 
 
 <?php
