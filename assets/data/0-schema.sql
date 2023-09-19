0998fbc7be830177fafaac5d8392cf3bd61775ff
João Netto <jovinettonetto@gmail.com>
2023-09-19 14:32:44 +0000

    insert data base

--- assets/data/0-schema.sql
+++ assets/data/0-schema.sql
@@ -21,3 +21,7 @@ CREATE TABLE usuarios(
 );
 
 
+INSERT INTO usuarios(nome, email, senha, nivel_acess) VALUES 
+('UnderSoundsADM','undersounds5@gmail.com','Undersounds2023', 3),
+('UserArt','UserArt@gmail.com','Art12345', 2),
+('UserNorm','UserNorm@gmail.com','Norm12345', 1);
