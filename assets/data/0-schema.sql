c80d0abcfb7331f06d65a294004312eea4046662
João Netto <jovinettonetto@gmail.com>
2023-09-19 15:00:19 +0000

    agora vai

--- assets/data/0-schema.sql
+++ assets/data/0-schema.sql
@@ -11,12 +11,12 @@ CREATE TABLE musicas(
 );
 
 CREATE TABLE usuarios(
-    us_id INT AUTO_INCREMENT PRIMARY KEY;
-    nome VARCHAR(200) NOT NULL;
-    email VARCHAR(200) NOT NULL;
-    senha VARCHAR(40) NOT NULL;
-    user_capa VARCHAR(200);
-    user_perfil VARCHAR(200);
+    us_id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    senha VARCHAR(40) NOT NULL,
+    user_capa VARCHAR(200),
+    user_perfil VARCHAR(200),
     nivel_acess INT NOT NULL
 );
 
--- assets/data/1-schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE usuarios(
-    user_id INT AUTO_INCREMENT PRIMARY KEY;
-    nome VARCHAR(200) NOT NULL;
-    email VARCHAR(200) NOT NULL;
-    senha VARCHAR(40) NOT NULL;
-    user_capa VARCHAR(200);
-    user_perfil VARCHAR(200);
-    nivel_acess INT NOT NULL
-);
